56b02cc616ba2e1dc2dd554e91fe4be8cd904ff7
riccardo sensi <riccardosensi57@gmail.com>
2024-02-28 12:44:45 +0100

    inizializzazione

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+
+
+<footer class="footer">
+    <div class="container-fluid">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-none text-white">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-none text-white">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-none text-white">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-none text-white">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-none text-white">About</a></li>
+        </ul>
+        <p class="text-center text-body-none text-white">© 2023 Company, Inc</p>
+
+    </div>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+    <h1 class=" text-center text-white mt-custom">Home</h1>
+    <div style="height: 1000px">
+
+    </div>
+
+</x-layout>
